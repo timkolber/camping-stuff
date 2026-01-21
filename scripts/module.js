675b8309f91aa9da1ea55967d5a9a02f87759cbb
Tim Kolber <112953640+timkolber@users.noreply.github.com>
2026-01-21 01:39:03 +0100

    Refactor module.js to use ES6 imports and hooks

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,28 @@
-Hooks.once('init', async function() {
+import { CampingGearConfig } from './config.js';
+import { DeploymentManager } from './deployment-manager.js';
+import { UIManager } from './ui-manager.js';
 
+Hooks.once('init', () => {
+  console.log('Camping Gear Deployment | Initializing');
+  CampingGearConfig.registerSettings();
 });
 
-Hooks.once('ready', async function() {
+Hooks.once('ready', () => {
+  console.log('Camping Gear Deployment | Ready');
+});
 
+Hooks.on('renderActorSheet', (app, html, data) => {
+  UIManager.addDeployButtons(app, html, data);
+});
+
+Hooks.on('renderTileConfig', (app, html, data) => {
+  UIManager.addPackButton(app, html, data);
+});
+
+Hooks.on('renderTileHUD', (hud, html, data) => {
+  UIManager.addHUDButton(hud, html, data);
+});
+
+Hooks.on('getTileDirectoryEntryContext', (html, options) => {
+  UIManager.addContextMenuOption(html, options);
 });
